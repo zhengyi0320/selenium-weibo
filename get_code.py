2e00d93275db90b96cd0753fafeb6ad77422bdd8
yzheng <45957413+zhengyi0320@users.noreply.github.com>
2019-03-14 16:16:26 +0800

    Update get_code.py

--- get_code.py
+++ get_code.py
@@ -4,12 +4,16 @@ from selenium import webdriver
 import tesserocr
 from PIL import Image
 
-screenImg = "F:/python_test/selenium_weibo/tesserocr/1.png"
+#截图储存的目录
+screenImg = "xxx.png"
 driver = webdriver.Chrome()
 driver.maximize_window()
+#打开一个验证码的url
 driver.get('http://my.cnki.net/elibregister/commonRegister.aspx#')
 time.sleep(3)
+#截取整个网页
 driver.get_screenshot_as_file(screenImg)
+#定位验证码所在位置
 location = driver.find_element_by_id('checkcode').location
 size = driver.find_element_by_id('checkcode').size
 left = location['x']
@@ -17,18 +21,21 @@ top =  location['y']
 right = location['x'] + size['width']
 bottom = location['y'] + size['height']
 time.sleep(1)
+#截取验证码图片
 img = Image.open(screenImg).crop((left,top,right,bottom))
 img.save(screenImg)
 image = Image.open(screenImg)
+#将图片转化为灰度图像
 image = image.convert('L')
-threshold = 127
+#指定二值化阈值
+threshold = 150
 table = []
 for i in range(256):
 	if i < threshold:
 		table.append(0)
 	else:
 		table.append(1)
-		
+#识别验证码		
 image = image.point(table, '1')
 result = tesserocr.image_to_text(image)
 print(result)	
