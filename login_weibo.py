ca9ef4c049707074ffbe7e3d8894e98fed33e050
yzheng <45957413+zhengyi0320@users.noreply.github.com>
2019-03-14 15:39:06 +0800

    Update login_weibo.py

--- login_weibo.py
+++ login_weibo.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 username = "xxxxx"
 password = "xxxxx"
-driver = webdriver.Chrome()
-driver.maximize_window()
 
 def login_weibo():
+	driver = webdriver.Chrome()
+	driver.maximize_window()
 	driver.get('http://www.weibo.com')
 	wait = WebDriverWait(driver, 10)
 	element = wait.until(EC.presence_of_element_located((By.ID, 'loginname')))
